a0a160a25502ed1944e28c3d6a6128d7435c6377
Kasturi Raghavan <kastur@gmail.com>
2012-04-10 16:11:36 -0700

    Revert "patched libcameraservice to fix build errors"
    This reverts commit 60163abc31ad1edaa87ba968372bcd78edf1c118.

--- services/camera/libcameraservice/Android.mk
+++ services/camera/libcameraservice/Android.mk
@@ -63,8 +63,4 @@ else
 LOCAL_SHARED_LIBRARIES += libcamera 
 endif
 
-ifeq ($(BOARD_USE_FROYO_LIBCAMERA), true)
-   LOCAL_CFLAGS += -DBOARD_USE_FROYO_LIBCAMERA
-endif
-
 include $(BUILD_SHARED_LIBRARY)
